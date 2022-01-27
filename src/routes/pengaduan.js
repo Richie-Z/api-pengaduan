b41dda726f758ab9ee235e5af231f53b40ead480
Richie-Z <zakariarichie@yahoo.com>
2022-01-27 19:01:43 +0700

    delete unneccesary field on get all, add get by id

--- src/routes/pengaduan.js
+++ src/routes/pengaduan.js
@@ -18,12 +18,33 @@ const upload = new Multer({ storage: storage });
 router.get("/", async function (req, res) {
   try {
     const pengaduanModel = await Pengaduan.findAll({
+      attributes: ["id", "laporan", "isiLaporan", "createdAt"],
       include: [
         {
           model: PengaduanDetail,
           as: "detail",
           where: { masyarakatIp: getIpClient(req.ip || req.ips) },
+          attributes: ["nama", "masyarakatIp", "status"],
         },
+      ],
+    });
+    res.json({
+      status: true,
+      message: "Success get",
+      data: pengaduanModel,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+router.get("/:pengaduanId(\\d+)", async function (req, res) {
+  try {
+    let { pengaduanId } = req.params;
+    const pengaduanModel = await Pengaduan.findByPk(pengaduanId, {
+      attributes: ["id", "laporan", "isiLaporan", "lampiran", "createdAt"],
+      include: [
+        "detail",
         {
           association: "tanggapan",
           include: "penanggap",
@@ -32,7 +53,7 @@ router.get("/", async function (req, res) {
     });
     res.json({
       status: true,
-      message: "Success get",
+      message: "Success get by ID",
       data: pengaduanModel,
     });
   } catch (error) {
@@ -58,7 +79,7 @@ router.post("/", upload.array("lampiran", 6), async (req, res) => {
       {
         masyarakatIp: getIpClient(req.ip),
         status: "belumVerif",
-        nama: nama
+        nama: nama,
       },
       { transaction: t }
     );
