91cece08dbb3168c6854329a72cec63acbc3beda
Richie-Z <zakariarichie@yahoo.com>
2022-02-07 14:06:06 +0700

    new pengaduan controller

---
+++ src/controller/PengaduanController.js
@@ -0,0 +1,95 @@
+import {
+  Pengaduan,
+  PengaduanDetail,
+  sequelize,
+} from "../database/models/index";
+import PengaduanException from "../exception/PengaduanException";
+
+const getIpClient = (ip) => ip.split(":").pop();
+
+const getPengaduanIP = async (req, res) => {
+  try {
+    const pengaduanModel = await Pengaduan.findAll({
+      attributes: ["id", "laporan", "isiLaporan", "createdAt"],
+      include: [
+        {
+          model: PengaduanDetail,
+          as: "detail",
+          where: { masyarakatIp: getIpClient(req.ip || req.ips) },
+          attributes: ["nama", "masyarakatIp", "status"],
+        },
+      ],
+    });
+    res.json({
+      status: true,
+      message: "Success get",
+      data: pengaduanModel,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+const getPengaduanID = async (req, res) => {
+  try {
+    let { pengaduanId } = req.params;
+    const pengaduanModel = await Pengaduan.findByPk(pengaduanId, {
+      attributes: ["id", "laporan", "isiLaporan", "lampiran", "createdAt"],
+      include: [
+        "detail",
+        {
+          association: "tanggapan",
+          include: "penanggap",
+        },
+      ],
+    });
+    if (!pengaduanModel)
+      throw new PengaduanException(
+        `Pengaduan with id ${pengaduanId} not found`
+      );
+    res.json({
+      status: true,
+      message: "Success get by ID",
+      data: pengaduanModel ?? "hello",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json(error);
+  }
+};
+
+const createPengaduan = async (req, res) => {
+  const t = await sequelize.transaction();
+  try {
+    const { isiLaporan, nama } = req.body;
+    const files = req.files.map((x) => x.path);
+    const pengaduanModel = await Pengaduan.create(
+      {
+        isiLaporan,
+        lampiran: JSON.stringify(files),
+        status: "belumVerif",
+      },
+      { transaction: t }
+    );
+    await pengaduanModel.createDetail(
+      {
+        masyarakatIp: getIpClient(req.ip),
+        status: "belumVerif",
+        nama: nama,
+      },
+      { transaction: t }
+    );
+    await t.commit();
+    res.json({
+      status: true,
+      message: "Create Pengaduan Success",
+      data: pengaduanModel,
+    });
+  } catch (error) {
+    await t.rollback();
+    console.log(error);
+    res.status(400).json(error);
+  }
+};
+export { getPengaduanIP, getPengaduanID, createPengaduan };
--- src/routes/pengaduan.js
+++ src/routes/pengaduan.js
@@ -1,104 +1,22 @@
 import { Router } from "express";
 import {
-  Pengaduan,
-  PengaduanDetail,
-  sequelize,
-} from "../database/models/index";
+  createPengaduan,
+  getPengaduanID,
+  getPengaduanIP,
+} from "../controller/PengaduanController";
 import Multer from "multer";
+
 const router = Router();
-const getIpClient = (ip) => ip.split(":").pop();
 const storage = Multer.diskStorage({
   destination: "./public/files",
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, `${file.fieldname}-${uniqueSuffix}-${file.originalname}`);
+    cb(null, `${file.fieldname}-${uniqueSuffix}`);
   },
 });
 const upload = new Multer({ storage: storage });
-function pengaduanException(msg){
-  this.status = false;
-  this.message = msg;
-  this.name = pengaduanException; 
-}
-router.get("/", async function (req, res) {
-  try {
-    const pengaduanModel = await Pengaduan.findAll({
-      attributes: ["id", "laporan", "isiLaporan", "createdAt"],
-      include: [
-        {
-          model: PengaduanDetail,
-          as: "detail",
-          where: { masyarakatIp: getIpClient(req.ip || req.ips) },
-          attributes: ["nama", "masyarakatIp", "status"],
-        },
-      ],
-    });
-    res.json({
-      status: true,
-      message: "Success get",
-      data: pengaduanModel,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
-});
-router.get("/:pengaduanId(\\d+)", async function (req, res) {
-  try {
-    let { pengaduanId } = req.params;
-    const pengaduanModel = await Pengaduan.findByPk(pengaduanId, {
-      attributes: ["id", "laporan", "isiLaporan", "lampiran", "createdAt"],
-      include: [
-        "detail",
-        {
-          association: "tanggapan",
-          include: "penanggap",
-        },
-      ],
-    });
-    if(!pengaduanModel) throw new pengaduanException(`Pengaduan with id ${pengaduanId} not found`)
-    res.json({
-      status: true,
-      message: "Success get by ID",
-      data: pengaduanModel?? "hello",
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(404).json(error);
-  }
-});
 
-router.post("/", upload.array("lampiran", 6), async (req, res) => {
-  const t = await sequelize.transaction();
-  try {
-    const { isiLaporan, nama } = req.body;
-    const files = req.files.map((x) => x.path);
-    const pengaduanModel = await Pengaduan.create(
-      {
-        isiLaporan,
-        lampiran: JSON.stringify(files),
-        status: "belumVerif",
-      },
-      { transaction: t }
-    );
-    await pengaduanModel.createDetail(
-      {
-        masyarakatIp: getIpClient(req.ip),
-        status: "belumVerif",
-        nama: nama,
-      },
-      { transaction: t }
-    );
-    await t.commit();
-    res.json({
-      status: true,
-      message: "Create Pengaduan Success",
-      data: pengaduanModel,
-    });
-  } catch (error) {
-    await t.rollback();
-    console.log(error);
-    res.status(400).json(error);
-  }
-});
+router.get("/", getPengaduanIP);
+router.get("/:pengaduanId(\\d+)", getPengaduanID);
+router.post("/", upload.array("lampiran", 6), createPengaduan);
 export default router;
