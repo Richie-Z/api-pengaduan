e02622ce9337145b23da2e2c345a5e598620d44f
Richie-Z <zakariarichie@yahoo.com>
2022-02-10 21:19:14 +0700

    add CSRF protection

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,21 +3,33 @@ import petugasRouter from "./petugas";
 import authRouter from "./auth";
 import pengaduanRouter from "./pengaduan";
 import auth from "../middleware/auth";
+import csurf from "csurf";
 
 const router = Router();
 const getIpClient = (ip) => ip.split(":").pop();
+const csrfProtection = csurf({ cookie: true });
 
 router.get("/", (_req, res) => {
   res.send("Hello World!");
 });
-router.get("/myip",(req,res)=>{
+
+router.get("/csrf-token", (req, res) => {
   res.json({
     status: true,
     message: "Success",
-    ip:getIpClient(req.ip || req.ips)
+    csrfToken: req.csrfToken(),
   });
 });
+
+router.get("/myip", (req, res) => {
+  res.json({
+    status: true,
+    message: "Success",
+    ip: getIpClient(req.ip || req.ips),
+  });
+});
+
 router.use("/auth", authRouter);
 router.use("/pengaduan", pengaduanRouter);
-router.use("/petugas", auth, petugasRouter);
+router.use("/petugas", auth, csrfProtection, petugasRouter);
 export default router;
