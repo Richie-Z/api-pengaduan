adc248e38ff05793da64a76cddcc7fbf6839d984
Richie-Z <zakariarichie@yahoo.com>
2022-03-14 20:42:26 +0700

    delete csrf secured routes

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,14 +1,28 @@
 import { Router } from "express";
+import csurf from "csurf";
+
 import pengaduanRouter from "./pengaduan";
-import csrfSecure from "./csrf-secured";
+import petugasRouter from "./petugas";
+import authRouter from "./auth";
+import adminController from "./admin";
+
+import auth from "../middleware/auth";
+import hasRole from "../middleware/Role";
 
 const router = Router();
+const csrfProtection = csurf({ cookie: true });
 const getIpClient = (ip) => ip.split(":").pop();
 
-router.get("/", (_req, res) => {
-  res.send("Hello World!");
+router.use(csrfProtection);
+router.use((err, _req, res, next) => {
+  if (err.code !== "EBADCSRFTOKEN") return next(err);
+  res.status(403);
+  res.json({ status: false, message: "Invalid CSRF Token" });
 });
 
+router.get("/", (_req, res) => {
+  res.send("THIS IS INDEX ENDPOINT FOR PENGADUAN!");
+});
 router.get("/myip", (req, res) => {
   res.json({
     status: true,
@@ -16,8 +30,19 @@ router.get("/myip", (req, res) => {
     ip: getIpClient(req.ip || req.ips),
   });
 });
+router.get("/csrf-token", (req, res) => {
+  const token = req.csrfToken();
+  res.cookie("XSRF-TOKEN", token);
+  res.json({
+    status: true,
+    message: "Success",
+    csrfToken: token,
+  });
+});
 
 router.use("/pengaduan", pengaduanRouter);
-router.use(csrfSecure);
+router.use("/auth", authRouter);
+router.use("/petugas", auth, petugasRouter);
+router.use("/admin", auth, hasRole("admin"), adminController);
 
 export default router;
